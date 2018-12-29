52887f7f1389da22323fee8c6cd88503074e3e33
DPROCODIGO <caparachindavid@gmail.com>
2018-12-28 19:32:22 -0500

    Esto se modifico

--- david/src/david/David.java
+++ david/src/david/David.java
@@ -5,7 +5,7 @@ public class David {
     public static void main(String[] args) {
         String nombre = "";
         if (nombre == "david") {
-            System.out.println("Hola joven "+nombre+ "dios");
+            System.out.println("Hola joven "+nombre+ "dios de los mejores que fue");
             
         }else{
             System.out.println("Lo lamento "+nombre+ " su nombre no pertenece al nombre que queria oir");
